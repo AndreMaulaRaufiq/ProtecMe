abb8a60b1ad25d86a58f8b5941523710bd33c7ab
andisety <purnomoandi126@gmail.com>
2023-02-28 22:42:18 +0700

    history get data from firebase

--- app/src/main/java/com/hermes/protecme/Artikel.kt
+++ app/src/main/java/com/hermes/protecme/Artikel.kt
@@ -1,18 +1,12 @@
 package com.hermes.protecme
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class Artikel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-        val intent = Intent (this, Pelecehan_BST ::class.java)
-        val bst = findViewById<ImageView>(R.id.pelecehan)
-        bst.setOnClickListener {
-            startActivity(intent)
-        }
+
     }
 }
