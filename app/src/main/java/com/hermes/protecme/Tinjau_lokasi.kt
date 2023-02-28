5e3a2650e3d812cbd44d6938cb630099db0b7040
muhraflisolo <muhraflioke@gmail,com>
2023-02-28 09:07:47 +0700

    Add History and detail history

---
+++ app/src/main/java/com/hermes/protecme/Tinjau_lokasi.kt
@@ -0,0 +1,18 @@
+package com.hermes.protecme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class Tinjau_lokasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+        val intent = Intent (this, Pelecehan_BST ::class.java)
+        val bst = findViewById<ImageView>(R.id.pelecehan)
+        bst.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+}
